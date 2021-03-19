33506fc70cebf65375e4d0bb6323eb1c099a04fa
Corrita <malbino001@gmail.com>
2021-03-18 23:32:08 -0300

    Primeira Aula

--- teste01.py
+++ teste01.py
@@ -1,1 +1,11 @@
-print('Hellow')
+
+
+lista = ['abc', 222, '740', [1,2,3]]
+#print (lista[2])
+
+dicionario = {'a':'val', 'b':123, 45:2.5}
+#print (dicionario['a'])
+
+for elem in range(1,10,2):
+    print(elem)
+
